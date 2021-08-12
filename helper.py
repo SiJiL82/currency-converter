e2f10dab0d02eaaa9e76b322cf0c0c3637b82806
Sven Lowry <sven.lowry@gmail.com>
2021-08-12 15:50:21 +0100

    Add function to get max dict key length

--- helper.py
+++ helper.py
@@ -3,7 +3,17 @@ def get_max_dict_value_length(dict):
     Returns the length of the longest value in a dictionary
     """
     max = 0
-    for value in dict:
-        if len(dict[value]) > max:
-            max = len(dict[value])
+    for key in dict:
+        if len(dict[key]) > max:
+            max = len(dict[key])
+    return max
+
+def get_max_dict_key_length(dict):
+    """
+    Returns the length of the longest value in a dictionary
+    """
+    max = 0
+    for key in dict:
+        if len(key) > max:
+            max = len(key)
     return max
