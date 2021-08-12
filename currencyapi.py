2602d158aec42cd85f9a8f073ad2ec9398f97d08
Sven Lowry <sven.lowry@gmail.com>
2021-08-12 15:52:52 +0100

    Add formatting to currency list display function

--- currencyapi.py
+++ currencyapi.py
@@ -1,6 +1,6 @@
 import json
 import requests
-import pprint
+import helper 
 
 def get_api_key():
     with open("apikey.json") as apikey_file:
@@ -18,11 +18,18 @@ def get_currency_list():
 
 def display_currency_list():
     symbols = json.loads(currency_list)["symbols"]
-    
+    i = 1
+    print_str = ""
+    #max key length plus spacing, plus max value length, plus spacing
+    column_width = helper.get_max_dict_key_length(symbols) + 2 + helper.get_max_dict_value_length(symbols) + 2
     for symbol in symbols:
-        print(f"{symbol}: {symbols.get(symbol)}")
-
+        str = f"{symbol}: {symbols.get(symbol)}"
+        if i == 4:
+            print_str += "\n"
+            i = 1
+        print_str += f"{str: <{column_width}}"
+        i += 1
+    print(print_str)
 
 get_currency_list()    
 display_currency_list()
- 
