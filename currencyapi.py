26d6cb92fb7ca378ea05401701a84bbd64f9d62a
Sven Lowry <sven.lowry@gmail.com>
2021-08-13 12:06:21 +0100

    Change to new API

--- currencyapi.py
+++ currencyapi.py
@@ -20,14 +20,11 @@ def get_currency_list():
     """
     Pull the latest list of available currencies from the API
     """
-    url = f"http://api.exchangeratesapi.io/v1/symbols?access_key={APIKEY}"
+    url = f"https://free.currconv.com/api/v7/currencies?apiKey={APIKEY}"
     response = requests.request("GET", url)
     # Store API result in the global variable for use in other functions
-    # This is a string value that needs converting to be used
-    currency_list_str = response.text
-    # Get just the symbol list from the currency list string
     global currency_list_dict
-    currency_list_dict = json.loads(currency_list_str)["symbols"]
+    currency_list_dict = json.loads(response.text)["results"]
 
 def display_currency_list():
     """
@@ -37,16 +34,16 @@ def display_currency_list():
     i = 1
     # Number of columns to print
     # TODO: See if we can get the console width, divide it by column_width and make this dynamic
-    num_columns = 4
+    num_columns = 3
     # Empty string to start the printed list with
     print_str = ""
     # Width of each column to print. This is:
     # max key length + spacing + max value length + spacing + colour code escape values
-    column_width = helper.get_max_dict_key_length(currency_list_dict) + 2 + helper.get_max_dict_value_length(currency_list_dict) + 2 + 20
+    column_width = helper.get_max_dict_subvalue_length(currency_list_dict, "currencyName") + 2 + helper.get_max_dict_value_length(currency_list_dict) + 2 + 20
     # Loop through each symbol to get out the key and value to print to console
     for currency in currency_list_dict:
         # String for each key and value, with colour formatting around the key
-        str = f"\033[1;32;40m{currency}\033[0;37;40m: {currency_list_dict.get(currency)}"
+        str = f"\033[1;32;40m{currency}\033[0;37;40m: {currency_list_dict.get(currency)['currencyName']}"
         # If we've appended as many strings as there are columns, reset the columns and start a new line
         if i > num_columns:
             print_str += "\n"
@@ -58,6 +55,14 @@ def display_currency_list():
     # Display the end string to the console.
     print(f"{print_str}")
 
+def get_exchange_rate(source, destination):
+    #url = f"http://api.exchangeratesapi.io/v1/latest?access_key={APIKEY}&base={source}&symbols={destination}"
+    url = "https://api.exchangeratesapi.io/v1/latest?access_key=0ad59c0762e230cbd9d500e6522ae0d0&base=USD&symbols=GBP,JPY,EUR"
+    print(url)
+    response = requests.request("GET", url)
+    response_object = json.loads(response.text)
+    print(response_object)
+
 # Debugging
-#get_currency_list()    
-#display_currency_list()
+get_currency_list()    
+display_currency_list()
