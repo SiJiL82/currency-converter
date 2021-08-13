82a6d7f42a123e07bd5406944437b7539f251deb
Sven Lowry <sven.lowry@gmail.com>
2021-08-13 11:58:41 +0100

    Add subkey length function to helper

--- helper.py
+++ helper.py
@@ -10,10 +10,21 @@ def get_max_dict_value_length(dict):
 
 def get_max_dict_key_length(dict):
     """
-    Returns the length of the longest value in a dictionary
+    Returns the length of the longest key in a dictionary
     """
     max = 0
     for key in dict:
         if len(key) > max:
             max = len(key)
+    return max
+
+def get_max_dict_subvalue_length(dict, key_name):
+    """
+    Returns the length of a the longest specified sub key in a dictionary
+    """
+    max = 0
+    for key in dict:
+        length = len(dict.get(key)[key_name])
+        if length > max:
+            max = length
     return max
