a427eb55a2af532f4f4c4110935e60e6e83389d8
Sven Lowry <sven.lowry@gmail.com>
2021-08-16 16:22:20 +0100

    Add debug logging

--- currencyconverter.py
+++ currencyconverter.py
@@ -19,6 +19,7 @@ def press_enter_to_continue():
     Loop until user presses Enter key, rather than jumping straight back to the menu.
     Makes it easier for user to see text returned by their operation.
     """
+    print("test")
     while True:
         input(f"{helper.blue_text}Press Enter to go back to the main menu.")
         break
