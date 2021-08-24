f0f2fd832a780a42db1b9eabe0cdd7adbd8a545d
Sven Lowry <sven.lowry@gmail.com>
2021-08-24 16:03:38 +0100

    Fix line lengths

--- currencyapi.py
+++ currencyapi.py
@@ -10,6 +10,7 @@ if os.path.exists("env.py"):
 # Store API key for use in other functions
 APIKEY = os.environ.get("APIKEY")
 
+
 # Global variable to store curency list in
 currency_list_dict = {}
 
@@ -35,29 +36,38 @@ def get_currency_list():
 def display_currency_list():
     """
     Prints the available currencies out in a user readable format
-    """   
+    """
     # Counter for number of columns in printed string
     i = 1
 
     # Width of each column to print. This is:
-    # max key length + spacing + max value length + spacing + colour code escape values
-    column_width = helper.get_max_dict_subvalue_length(currency_list_dict, "currencyName") + 2 + helper.get_max_dict_value_length(currency_list_dict) + 2 + 20
+    # max key length + spacing + max value length
+    # + spacing + colour code escape values
+    value_length = helper.get_max_dict_subvalue_length(currency_list_dict,
+                                                       "currencyName")
+    key_length = helper.get_max_dict_value_length(currency_list_dict)
+    colour_format_length = 20
+    column_width = value_length + 2 + key_length + 2 + colour_format_length
 
     # Number of columns to print
     terminal_columns, terminal_rows = shutil.get_terminal_size()
-    num_columns = math.floor(terminal_columns / (column_width - 20))
+    num_columns = math.floor(terminal_columns /
+                             (column_width - colour_format_length))
 
     # Empty string to start the printed list with
     print_str = ""
     # Loop through each symbol to get out the key and value to print to console
     for currency in currency_list_dict:
         # String for each key and value, with colour formatting around the key
-        str = f"{helper.green_text}{currency}{helper.white_text}: {currency_list_dict.get(currency)['currencyName']}"
-        # If we've appended as many strings as there are columns, reset the columns and start a new line
+        str = f"{helper.green_text}{currency}{helper.white_text}: \
+{currency_list_dict.get(currency)['currencyName']}"
+        # If we've appended as many strings as there are columns,
+        # reset the columns and start a new line
         if i > num_columns:
             print_str += "\n"
             i = 1
-        # Add the key/value pair string to the overall string, with the column width formatting
+        # Add the key/value pair string to the overall string,
+        # with the column width formatting
         print_str += f"{str: <{column_width}}"
         # Increment column counter
         i += 1
@@ -74,7 +84,8 @@ def get_exchange_rate(source, destination):
     # Set string for reverse of search
     reverse_lookup = f"{destination}_{source}"
     # Set API request URL
-    url = f"{api_url}/convert?q={lookup},{reverse_lookup}&compact=ultra&apiKey={APIKEY}"
+    url = f"{api_url}/convert?q={lookup},{reverse_lookup}\
+&compact=ultra&apiKey={APIKEY}"
     # Get the API response
     response = requests.request("GET", url)
     exchange_rate = json.loads(response.text)
@@ -90,9 +101,13 @@ def display_exchange_rate(source, destination, exchange_rate):
     # Set key for reverse of search
     reverse_lookup = f"{destination}_{source}"
     # Display provided search result to user
-    print(f"1 {helper.green_text}{source}{helper.white_text} is equal to {exchange_rate.get(lookup)} {helper.green_text}{destination}{helper.white_text}")
+    print(f"1 {helper.green_text}{source}{helper.white_text} is equal to \
+{exchange_rate.get(lookup)} {helper.green_text}{destination}\
+{helper.white_text}")
     # Display reverse of search to user
-    print(f"1 {helper.green_text}{destination}{helper.white_text} is equal to {exchange_rate.get(reverse_lookup)} {helper.green_text}{source}{helper.white_text}")
+    print(f"1 {helper.green_text}{destination}{helper.white_text} is equal to \
+{exchange_rate.get(reverse_lookup)} {helper.green_text}{source}\
+{helper.white_text}")
 
 
 def convert_currency(source, destination, amount, exchange_rate):
@@ -116,7 +131,9 @@ def display_converted_currency(source, destination, amount, amount_converted):
     source_currency_symbol = get_currency_symbol(source)
     destination_currency_symbol = get_currency_symbol(destination)
     # Display to user
-    print(f"{source_currency_symbol}{amount} {helper.green_text}{source}{helper.white_text} = {destination_currency_symbol}{amount_converted} {helper.green_text}{destination}{helper.white_text}")
+    print(f"{source_currency_symbol}{amount} {helper.green_text}{source}\
+{helper.white_text} = {destination_currency_symbol}{amount_converted} \
+{helper.green_text}{destination}{helper.white_text}")
 
 
 def get_currency_symbol(key):
